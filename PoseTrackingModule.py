5a0c9bd298a0c3445bb3d8ca571c57c34379eb2c
OrangeX4 <318483724@qq.com>
2022-10-11 00:08:45 +0800

    feat: add some functions

--- PoseTrackingModule.py
+++ PoseTrackingModule.py
@@ -1,63 +1,11 @@
 import cv2
-from keyboard import wait
 import mediapipe as mp
 import time
-import autopy
+import pydirectinput
+# https://github.com/learncodebygaming/pydirectinput
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
-
-
-import ctypes
-
-SendInput = ctypes.windll.user32.SendInput
-
-# C struct redefinitions 
-PUL = ctypes.POINTER(ctypes.c_ulong)
-class KeyBdInput(ctypes.Structure):
-    _fields_ = [("wVk", ctypes.c_ushort),
-                ("wScan", ctypes.c_ushort),
-                ("dwFlags", ctypes.c_ulong),
-                ("time", ctypes.c_ulong),
-                ("dwExtraInfo", PUL)]
-
-class HardwareInput(ctypes.Structure):
-    _fields_ = [("uMsg", ctypes.c_ulong),
-                ("wParamL", ctypes.c_short),
-                ("wParamH", ctypes.c_ushort)]
-
-class MouseInput(ctypes.Structure):
-    _fields_ = [("dx", ctypes.c_long),
-                ("dy", ctypes.c_long),
-                ("mouseData", ctypes.c_ulong),
-                ("dwFlags", ctypes.c_ulong),
-                ("time",ctypes.c_ulong),
-                ("dwExtraInfo", PUL)]
-
-class Input_I(ctypes.Union):
-    _fields_ = [("ki", KeyBdInput),
-                 ("mi", MouseInput),
-                 ("hi", HardwareInput)]
-
-class Input(ctypes.Structure):
-    _fields_ = [("type", ctypes.c_ulong),
-                ("ii", Input_I)]
-
-# Actuals Functions
-
-def PressKey(hexKeyCode):
-    extra = ctypes.c_ulong(0)
-    ii_ = Input_I()
-    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra) )
-    x = Input( ctypes.c_ulong(1), ii_ )
-    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
-
-def ReleaseKey(hexKeyCode):
-    extra = ctypes.c_ulong(0)
-    ii_ = Input_I()
-    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra) )
-    x = Input( ctypes.c_ulong(1), ii_ )
-    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
 
 
 def handle_direction(lm):
@@ -91,6 +39,11 @@ def handle_direction(lm):
     elif lm[0].x > (lm[7].x - ear_distance * lr_error_rate):
         # lm[0] x 轴方向大于 lm[7] x 轴方向，说明头部向左转
         print('Left')
+        x, y = pydirectinput.position()
+        print('before:', x, y)
+        pydirectinput.moveTo(x - 100)
+        x, y = pydirectinput.position()
+        print('after:', x, y)
         return 'Left'
     elif lm[0].x < (lm[8].x + ear_distance * lr_error_rate):
         # lm[0] x 轴方向小于 lm[8] x 轴方向，说明头部向右转
@@ -153,27 +106,21 @@ def handle_running_and_jump(lm):
         if max_y - min_y > ear_distance * jump_threshold:
             # print('Jump')
             # press('space')
-            if is_running:
-                ReleaseKey(0x11)
-                is_running = False
-            # time.sleep(0.1)
-            # press('space')
-            PressKey(0x39)
-            ReleaseKey(0x39)
+            pydirectinput.press('space')
             return 'Jump'
         elif max_y - min_y > ear_distance * run_threshold:
             # print('Run')
-            # autopy.key.toggle('w', True, []) 
             if not is_running:
-                PressKey(0x11)
+                # PressKey(0x11)
+                pydirectinput.keyDown('w')
                 is_running = True
             return 'Run'
         else:
-            # autopy.key.toggle('w', False, [])
+            # print('Stand')
             if is_running:
-                ReleaseKey(0x11)
+                pydirectinput.keyUp('w')
+                # ReleaseKey(0x11)
                 is_running = False
-            print('Stand')
             return 'Stand'
 
 
